ad17dfecb4f40789a1b46a66ef9e5ddba7d22fa7
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-23 18:25:21 +0200

    added with ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,17 +2,15 @@ import React, { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/contactsSlise';
+import css from './ContactList.module.css';
 
 const ContactList = () => {
   const filters = useSelector(state => state.filters);
   const contacts = useSelector(state => state.contacts);
   localStorage.setItem('user-contact', JSON.stringify(contacts));
-  console.log('contacts', contacts);
-  console.log('filters', filters);
 
   const contactss = JSON.parse(localStorage.getItem('user-contact'));
 
-  console.log('contactss', contactss);
   const visibleFilter = contactss.filter(contact =>
     contact.name.toLowerCase().includes(filters)
   );
@@ -30,7 +28,12 @@ const ContactList = () => {
       {visibleFilter.map(contact => (
         <li key={contact.id}>
           {contact.name}: {contact.number}{' '}
-          <button onClick={() => handleDelete(contact.id)}>Delete</button>
+          <button
+            className={css.delete}
+            onClick={() => handleDelete(contact.id)}
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
